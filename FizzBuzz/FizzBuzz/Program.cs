fed5130d2f5c213f3c8145492ce5cfccbf0c13f7
singha4 <amandeep159632478@gmail.com>
2014-02-20 10:05:18 -0500

    version 1

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+
+                    System.Console.WriteLine(i);
+
+                }
+
+                if (i % 3 == 0)
+                {
+
+                    System.Console.WriteLine("Fizz");
+
+                }
+
+                if (i % 5 == 0)
+                {
+
+                    System.Console.WriteLine(i);
+
+                }
+
+                if (i % 5 == 0)
+                {
+
+                    System.Console.WriteLine("Buzz");
+
+                }
+            }
+        }
+    }
+}
